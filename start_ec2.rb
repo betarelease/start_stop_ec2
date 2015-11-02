9b65fc8a93ecd0c614cfdf50143854cc4c50486a
Sudhindra Rao <sudhindra.r.rao@gmail.com>
2015-11-02 12:07:30 -0800

    added system status option

--- start_ec2.rb
+++ start_ec2.rb
@@ -4,24 +4,30 @@ command = ARGV[0]
 instance_id = ARGV[1]
 
 ec2 = Aws::EC2::Client.new(region: 'us-east-1')
+
 case command
 when 'stop'
-puts "[DEBUG] stopping instance #{instance_id}"
-response = ec2.stop_instances({
-  instance_ids: [instance_id],
-  force: true
-})
-puts"[DEBUG] stopped instance #{instance_id}"
-puts "[DEBUG] response => #{response.inspect}"
+  puts "stopping instance #{instance_id}"
+  response = ec2.stop_instances({
+    instance_ids: [instance_id],
+    force: true
+  })
+  puts "stopped instance #{instance_id}"
+  puts "response => #{response.inspect}"
 
 when 'start'
-puts "[DEBUG] starting instance #{instance_id}"
-response = ec2.start_instances({
-  instance_ids: [instance_id]
-})
-puts "[DEBUG] started instance #{instance_id}"
-puts "[DEBUG] response => #{response.inspect}"
+  puts "starting instance #{instance_id}"
+  response = ec2.start_instances(
+    instance_ids: [instance_id])
+  puts "started instance #{instance_id}"
+  puts "response => #{response.inspect}"
+
+when 'status'
+  response = ec2.describe_instance_status(
+    instance_ids: [instance_id])
+  puts "status of instance #{instance_id} : #{response.instance_statuses[0].instance_state.name}"
+  puts "response => #{response.inspect}"
 
 else
-  puts"[DEBUG] no command specified"
+  puts 'no command specified'
 end
